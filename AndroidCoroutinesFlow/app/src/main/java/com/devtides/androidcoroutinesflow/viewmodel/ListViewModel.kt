c5c2944968a88280da19e207c72133313dd86cfd
prasad111 <prasad.mohanty2011@gmail.com>
2021-06-02 22:12:14 +0100

    Added android coroutines flow example with retrofit

---
+++ AndroidCoroutinesFlow/app/src/main/java/com/devtides/androidcoroutinesflow/viewmodel/ListViewModel.kt
@@ -0,0 +1,12 @@
+package com.devtides.androidcoroutinesflow.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import com.devtides.androidcoroutinesflow.model.NewsRepository
+
+class ListViewModel: ViewModel() {
+
+    val newsArticles = NewsRepository().getNewsArticles().asLiveData()
+
+}
